5a418577737270cb969ebb5e02601b94b6d0d584
emsj06001 <emsj06001@E-PW024NWP>
2023-09-06 10:49:34 +0200

    Add project files.

---
+++ Uppgift 1.3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Uppgift_1_3
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   /\\");
+            Console.WriteLine("  /  \\");
+            Console.WriteLine(" / \"\" \\");
+            Console.WriteLine("/______\\");
+            Console.ReadKey();
+        }
+    }
+}
